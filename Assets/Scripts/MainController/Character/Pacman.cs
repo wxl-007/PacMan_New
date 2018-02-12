cf15ce93783e8b500b49fb09c55c575d3bfc1ab7
wxl-007 <251280943@qq.com>
2018-02-12 01:50:13 -0500

    add methods
     for changing way and move

--- Assets/Scripts/MainController/Character/Pacman.cs
+++ Assets/Scripts/MainController/Character/Pacman.cs
@@ -4,22 +4,82 @@ using UnityEngine;
 
 public class Pacman : MonoBehaviour {
 
-	enum Orentation{
-		Left=1,
-		Right,
-		Up,
+	enum Direction{
+		Up=1,
+		Left,
 		Down,
+		Right,
+		non,
 	}
 	[HideInInspector]
-	Orentation curOren= Orentation.Left;
-
-	// Use this for initialization
-	void Start () {
-		
+	Direction curDir= Direction.non;
+	public int CurDir {
+		get{ return (int)curDir;}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	float speed = 3.0f;
+	Direction waitingDir = Direction.non;
+	//is super or not 
+	bool curState;
+	float timer =0;
+	public void Init(){
+		curDir = Direction.Left;
+		curState = false;
+		waitingDir = Direction.non;
+	}
+	public void SetDirection(int pDir ){
+		//stop first 
+		if (pDir == 5) {
+			
+		}
+		//oppsite direciton == 2 
+		if (Mathf.Abs ((int)curDir - pDir) != 2) {
+			waitingDir = (Direction)pDir;
+		} else {
+			//directly change dont wait 
+			curDir =(Direction)pDir;
+			ChangeDirection ();
+		}
+	}
+	public void SetState(bool pState){
+		curState = pState;
+	}
+
+	//change dir  
+	void ChangeDirection(){
+		if (curDir == Direction.Up) {
+			this.transform.localEulerAngles = Vector3.back * 90f;
+		} else if (curDir == Direction.Left) {
+			this.transform.localEulerAngles = Vector3.zero;
+		} else if (curDir == Direction.Down) {
+			this.transform.localEulerAngles = Vector3.forward * 90;
+		} else if (curDir == Direction.Right) {
+			this.transform.localEulerAngles = Vector3.forward * 180;
+		}
+	}
+	/// <summary>
+	/// Offsets the position when pacman is changing the dir , it should be in the center of the corner 
+	/// </summary>
+	void OffsetPos(){
+
+
+	}
+	void Update(){
+		timer += Time.deltaTime;
+
+		if (timer % 3 == 0) {
+			if (curDir == Direction.Left) {
+				this.gameObject.transform.localPosition += Vector3.left * speed;
+			} else if (curDir == Direction.Right) {
+				this.gameObject.transform.localPosition += Vector3.right * speed;
+			} else if (curDir == Direction.Up) {
+				this.gameObject.transform.localPosition += Vector3.up * speed;
+			} else if (curDir == Direction.Down) {
+				this.gameObject.transform.localPosition += Vector3.down * speed;
+			} else {
+			
+			
+			}
+		}
+
 	}
 }
--- Assets/Scripts/MainController/GameController.cs
+++ Assets/Scripts/MainController/GameController.cs
@@ -16,6 +16,7 @@ public class GameController : MonoBehaviour {
 
 	[HideInInspector]
 	public GameObject pacmanObj;
+	Pacman PacCtrl; 
 	public void Init(Object pMainCtrl){
 		m_MainObj = (MainController)pMainCtrl;
 		m_Map = this.transform.Find ("Panel/MapParentObj").GetComponent<Map>();
@@ -37,20 +38,82 @@ public class GameController : MonoBehaviour {
 	//
 	void InitPacman(){
 		pacmanObj.SetActive (true);
+		// <-
 		pacmanObj.transform.localEulerAngles = Vector3.zero;
 		pacmanObj.transform.localScale = Vector3.one;
 		pacmanObj.transform.localPosition = new Vector3(m_Map.bornPoint[1]*47,m_Map.bornPoint[0]*(-47),20)  ;
+		PacCtrl = pacmanObj.GetComponent<Pacman> ();
+		PacCtrl.Init ();
 	}
 	void CalculateScore(){
 		//count score and add lives
 	}
+	/// <summary>
+	///  locating where the object is in the matrix
+	/// </summary>
+	int[,] Locator(GameObject pObj){
+		Vector3 tPos =  pObj.transform.localPosition;
+		return new int[,] {{Mathf.CeilToInt (tPos.x / 47), Mathf.CeilToInt (tPos.y / 47)} };
+	}
+	/// <summary>
+	/// Determines whether this instance can move the specified pObj pDirNum.
+	/// </summary>
+	/// <returns><c>true</c> if this instance can move the specified pObj pDirNum; otherwise, <c>false</c>.</returns>
+	/// <param name="pObj">P object.</param>
+	/// <param name="pDirNum">P dir number.</param>
+	bool CanMove(GameObject pObj,int pDirNum){
+		int[,] tPos = Locator (pObj); 
+		if (pDirNum == 1) {
+			//up  y-1 
+			if (mapArr [tPos [0, 0], tPos [0, 1] - 1] > 0)
+				return false;
+			else
+				return true;
+		} else if (pDirNum == 2) {
+			//left x-1
+			if (mapArr [tPos [0, 0] - 1, tPos [0, 1]] > 0)
+				return false;
+			else
+				return true;
+		} else if (pDirNum == 3) {
+			//down y+1
+			if (mapArr [tPos [0, 0], tPos [0, 1] + 1] > 0)
+				return false;
+			else
+				return true;
+		} else if (pDirNum == 4) {
+			// right x+1
+			if(mapArr [tPos [0, 0] + 1, tPos [0, 1]] > 0)
+				return false;
+			else
+				return true;
+		}
+		return false;
+	
+	}
+
 
 	void Update(){
 		timer += Time.deltaTime;
 		if (timer % 3 == 0) {
-			
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				// up
+				if(CanMove(pacmanObj,1))
+					PacCtrl.SetDirection(1);
+			} else if (Input.GetKey (KeyCode.DownArrow)) {
+				if(CanMove(pacmanObj,3))
+					PacCtrl.SetDirection(3);
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				if(CanMove(pacmanObj,2))
+					PacCtrl.SetDirection(2);
+			} else if (Input.GetKey (KeyCode.RightArrow)) {
+				if(CanMove(pacmanObj,4))
+					PacCtrl.SetDirection(4);
+			}
+			if (CanMove (pacmanObj, PacCtrl.CurDir) == false) {
+				//stop
+				PacCtrl.SetDirection (5);
+			}
 		}
-
 	}
-
 }
